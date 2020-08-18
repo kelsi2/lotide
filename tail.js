8ce4a33c59a0933d3bb211775fcfbe1ecacca8a1
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-08-18 03:57:17 +0000

    added tail function

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${arguments[0]} === ${arguments[1]}`);
+  } else if (actual !== expected) {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion failed: ${arguments[0]} !== ${arguments[1]}`);
+  }
+};
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+// Test Case: Check the original array
+/*const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+*/
+
+const words = ["one"];
+tail(words);
+assertEqual(words.length, 1);
