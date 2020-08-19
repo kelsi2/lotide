87a1daa700cac40e7a0ba446924bb5a5f2dbbdb0
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-08-19 23:45:03 +0000

    add findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed: ${arguments[0]} === ${arguments[1]}`);
+  } else if (actual !== expected) {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion failed: ${arguments[0]} !== ${arguments[1]}`);
+  }
+};
+
+//scan object and return first key which contains value
+//if no key found, return undefined
+
+const findKeyByValue = function(obj, value) {
+  let result = "";
+
+  for (const key in obj) {
+    if (obj[key] === value) {
+      result = key;
+    }
+  }
+  if (result === "") {
+    return undefined;
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); //PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); //PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama"); //FAIL
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "sci-fi"); //FAIL
