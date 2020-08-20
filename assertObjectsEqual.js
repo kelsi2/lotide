b3ff3426a426e71a95b7163a73a20571068d83be
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-08-20 02:46:15 +0000

    add assertObjectsEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-
-  if (arr1.length !== arr2.length) {
-    result = false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      result = false;
-    }
-  }
-  return result;
-};
-
-
-//Returns true if both objects have identical keys and values, else returns false
 const eqObjects = function(object1, object2) {
   let obj1Length = Object.keys(object1).length;
   let obj2Length = Object.keys(object2).length;
@@ -39,16 +22,21 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log((`${String.fromCodePoint(0x2705)} Assertion Passed: ${inspect(arguments[0])} === ${inspect(arguments[1])}`));
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion failed: ${inspect(arguments[0])} !== ${inspect(arguments[1])}`);
+  }
+};
+
+// ----------- TEST CASES -----------
+
 const ab = {a: "1", b: "2"};
 const ba = {b: "2", a: "1"};
-console.log(eqObjects(ab, ba)); // true
+console.log(assertObjectsEqual(ab, ba)); // true
 
 const abc = {a: "1", b: "2", c: "3"};
-console.log(eqObjects(ab, abc)); // false
-
-const cd = {c: "1", d: ["2", 3]};
-const dc = {d: ["2", 3], c: "1"};
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = {c: "1", d: ["2", 3, 4]};
-console.log(eqObjects(cd, cd2)); // => false
+console.log(assertObjectsEqual(ab, abc)); // false
